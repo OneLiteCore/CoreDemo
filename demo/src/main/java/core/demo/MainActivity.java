a3fa65508555f4fdbef2758961961731e1d4613a
DrkCore <178456643@qq.com>
2016-09-27 00:09:11 +0800

    修改获取Frag的方式

--- demo/src/main/java/core/demo/MainActivity.java
+++ demo/src/main/java/core/demo/MainActivity.java
@@ -1,47 +1,41 @@
 package core.demo;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.List;
 
+import core.demo.frag.FragFactory;
 import core.mate.adapter.SimpleAdapter;
 import core.mate.adapter.SimpleViewHolder;
 import core.mate.app.CoreActivity;
-import core.mate.util.ClassUtil;
 
 public class MainActivity extends CoreActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
 
-        ListView listView = (ListView) findViewById(R.id.listView_main);
+		ListView listView = (ListView) findViewById(R.id.listView_main);
+		List<Class> frags = FragFactory.getFrags();
+		listView.setAdapter(new SimpleAdapter<Class>(android.R.layout.simple_list_item_1, frags) {
+			@Override
+			protected void bindViewData(SimpleViewHolder<Class> holder, int position, Class data, int viewType) {
+				TextView textView = holder.getCastView();
+				textView.setText(data.getSimpleName());
+			}
+		});
 
-        try {//动态获取activity包下的所有Frag
-            List<Class> frags = ClassUtil.getSubClassUnderPackage(Fragment.class, "core.demo.frag");
-            listView.setAdapter(new SimpleAdapter<Class>(android.R.layout.simple_list_item_1, frags) {
-                @Override
-                protected void bindViewData(SimpleViewHolder<Class> holder, int position, Class data, int viewType) {
-                    TextView textView = holder.getCastView();
-                    textView.setText(data.getSimpleName());
-                }
-            });
-        } catch (Throwable e) {
-            e.printStackTrace();
-        }
-
-        //点击进入Activity
-        listView.setOnItemClickListener((parent, view, position, id) -> {
-            SimpleAdapter<Class> adapter = (SimpleAdapter<Class>) parent.getAdapter();
-            Class clz = adapter.getItem(position);
-            FragActivity.start(this, clz);
-        });
-    }
+		//点击进入Activity
+		listView.setOnItemClickListener((parent, view, position, id) -> {
+			SimpleAdapter<Class> adapter = (SimpleAdapter<Class>) parent.getAdapter();
+			Class clz = adapter.getItem(position);
+			FragActivity.start(this, clz);
+		});
+	}
 }
